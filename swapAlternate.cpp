491f9477ec6b25a41b1030bff7baa8aa7259f86a
pinkoyes <dotpriyanshu@gmail.com>
2024-11-07 00:26:05 +0530

    swap alternate position of an array

---
+++ swapAlternate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int size) {
+    for (int i=0; i<size; i+=2) {
+        if (i+1 < size) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+}
+
+void displayArray(int arr[], int size) {
+    for (int i=0; i<size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int n;
+    cout << "enter length of an array: ";
+    cin >> n;
+
+    // declaring array as (int arr[n]) is bad practice
+    int arr[n];
+
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    swapAlternate(arr, n);
+    displayArray(arr, n);
+}
